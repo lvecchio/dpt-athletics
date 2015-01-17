8e6f719a94c8327ecdc9baf26dba5d58bf20fb46
Dan Lourenco <daniel.lourenco@fmr.com>
2015-01-17 17:36:05 -0500

    add measurements ctrl

--- src/app/states/measurements/measurementsController.js
+++ src/app/states/measurements/measurementsController.js
@@ -1,10 +1,1 @@
-'use strict';
-
-
-function MeasurementsController () {
-  var vm = this;
-
-  this.page = 'Measurements Controller';
-}
-
-angular.module('app').controller('MeasurementsController', MeasurementsController);
+'use strict';function MeasurementsController () {  var vm = this;  this.page = 'Measurements Controller';}angular.module('app').controller('MeasurementsController', MeasurementsController);
