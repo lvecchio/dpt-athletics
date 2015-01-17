0d206c64cbe566c438a65c2a8df7b3a167a34cef
Laura Vecchio <laura.m.vecchio@gmail.com>
2015-01-17 18:12:44 -0500

    Migrating register functionality to firebase service

--- src/app/services/firebase.js
+++ src/app/services/firebase.js
@@ -5,13 +5,51 @@
  * @desc Service for all Firebase logic
  * @doc https://github.com/toddmotto/angularjs-styleguide#services-and-factory
  */
-function FirebaseService () {
+function FirebaseService ($firebase, $firebaseAuth, FIREBASE_URL) {
+
   var FirebaseService = {};
 
-  FirebaseService.someValue = '';
-  FirebaseService.someMethod = function () {
+  // create firebase references
+  FirebaseService.ref = new Firebase(FIREBASE_URL);
+  FirebaseService.authObj = $firebaseAuth(FirebaseService.ref);
+
+  /**
+   * @name createUser
+   * @desc Creates new user in "users" object in Firebase
+   * @param {string} email - User's email to create account with
+   * @param {password} password - User's password
+   */
+  FirebaseService.createUser = function (email, password) {
+
+    FirebaseService.authObj.$createUser(email, password).then(function(){
+      console.log("User created successfully");
+
+      // Once user is created, automatically log user into app
+      return FirebaseService.authObj.$authWithPassword({
+        email: email,
+        password: password
+      });
+
+    }).then(function(authData){
+
+      console.log("Logged in as: ", authData.uid);
+      console.log(authData);
+
+      // create user record with uid & email
+      var newUser = {
+        email: authData.password.email
+      };
+
+      // store user data in profile object
+      var profileRef = $firebase(FirebaseService.ref.child('users'));
+      profileRef.$set(authData.uid, newUser);
+
+    }).catch(function(error) {
+      console.log("Error: ", error);
+    });
 
   };
+
   return FirebaseService;
 }
 
--- src/app/states/register/registerController.js
+++ src/app/states/register/registerController.js
@@ -5,42 +5,15 @@
  * @desc Controller for the register screen.
  * @doc https://github.com/toddmotto/angularjs-styleguide#controllers
  */
-function RegisterController($firebase, $firebaseAuth, FIREBASE_URL, $state) {
+function RegisterController(FirebaseService, $state) {
 
   // create reference to view model
   var register = this;
 
-  // create firebase references
-  register.ref = new Firebase(FIREBASE_URL);
-  register.authObj = $firebaseAuth(register.ref);
+  // registerUser method
+  register.registerUser = function() {
 
-  // createUser method
-  register.createUser = function() {
-
-    register.authObj.$createUser(register.email, register.password).then(function(){
-      console.log("User created successfully");
-
-      return register.authObj.$authWithPassword({
-        email: register.email,
-        password: register.password
-      });
-
-    }).then(function(authData){
-      console.log("Logged in as: ", authData.uid);
-      console.log(authData);
-
-      // create user record with uid & email
-      var newUser = {
-        email: authData.password.email
-      };
-
-      // store user data in profile object
-      var profileRef = $firebase(register.ref.child('users'));
-      profileRef.$set(authData.uid, newUser);
-
-    }).catch(function(error) {
-      console.log("Error: ", error);
-    });
+    FirebaseService.createUser(register.email, register.password);
 
     // switch to dashboard state
     $state.go('dashboard');
