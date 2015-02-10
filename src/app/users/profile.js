5db53af20aaa0606b02b33e600f052e7bf91ccaf
Laura Vecchio <laura.m.vecchio@gmail.com>
2015-02-09 19:44:41 -0500

    Profile styling

--- src/app/users/profile.js
+++ src/app/users/profile.js
@@ -12,10 +12,13 @@
 
     // current user
     vm.user = {
-      name: 'Laura Vecchio',
+      firstName: 'Laura',
+      lastName: 'Vecchio',
       bodyFat: '25',
       fitnessLevel: 'Blue',
       classesAttended: '78'
     }
+
+    //TODO: Create method to save profile data back to Firebase
   }
 })();
